80d0446906528d22b9878adf6482a91b98e8bf83
YagiGD <its.charbs@gmail.com>
2026-02-22 18:45:14 -0500

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -67,7 +67,7 @@ export default {
                                 <img v-if="record.mobile" :src="\`/assets/phone-landscape\${store.dark ? '-dark' : ''}.svg\`" alt="Mobile">
                             </td>
                             <td class="hz">
-                                <p>{{ record.hz }}Hz</p>
+                                <p>Group {{ group }}</p>
                             </td>
                         </tr>
                     </table>
